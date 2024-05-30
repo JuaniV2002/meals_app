7de14d5b85aecb0641c13c62fcc3dd2c14fa81df
JuaniV2002 <juani2002villa@icloud.com>
2024-05-29 23:59:34 -0300

    cupertino styled switches in fliters screen

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -51,80 +51,81 @@ class _FilterScreenState extends State<FilterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Filters'),
-          actions: [
-            IconButton(
-              icon: const Icon(CupertinoIcons.floppy_disk),
-              onPressed: () {
-                final selectedFilters = {
-                  'gluten': _glutenFree,
-                  'lactose': _lactoseFree,
-                  'vegan': _vegan,
-                  'vegetarian': _vegetarian,
-                };
-                widget.saveFilters(selectedFilters);
-              },
+      appBar: AppBar(
+        title: const Text('Filters'),
+        actions: [
+          IconButton(
+            icon: const Icon(CupertinoIcons.floppy_disk),
+            onPressed: () {
+              final selectedFilters = {
+                'gluten': _glutenFree,
+                'lactose': _lactoseFree,
+                'vegan': _vegan,
+                'vegetarian': _vegetarian,
+              };
+              widget.saveFilters(selectedFilters);
+            },
+          ),
+        ],
+      ),
+      drawer: MainDrawer(),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.titleLarge,
             ),
-          ],
-        ),
-        drawer: MainDrawer(),
-        body: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(20),
-              child: Text(
-                'Adjust your meal selection',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchListTile(
+                  'Gluten-free',
+                  'Only include gluten-free meals',
+                  _glutenFree,
+                  (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Lactose-free',
+                  'Only include lactose-free meals',
+                  _lactoseFree,
+                  (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Vegetarian',
+                  'Only include vegetarian meals',
+                  _vegetarian,
+                  (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Vegan',
+                  'Only include vegan meals',
+                  _vegan,
+                  (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  },
+                ),
+              ],
             ),
-            Expanded(
-              child: ListView(
-                children: [
-                  _buildSwitchListTile(
-                    'Gluten-free',
-                    'Only include gluten-free meals',
-                    _glutenFree,
-                    (newValue) {
-                      setState(() {
-                        _glutenFree = newValue;
-                      });
-                    },
-                  ),
-                  _buildSwitchListTile(
-                    'Vegetarian',
-                    'Only include vegetarian meals',
-                    _vegetarian,
-                    (newValue) {
-                      setState(() {
-                        _vegetarian = newValue;
-                      });
-                    },
-                  ),
-                  _buildSwitchListTile(
-                    'Vegan',
-                    'Only include vegan meals',
-                    _vegan,
-                    (newValue) {
-                      setState(() {
-                        _vegan = newValue;
-                      });
-                    },
-                  ),
-                  _buildSwitchListTile(
-                    'Lactose-free',
-                    'Only include lactose-free meals',
-                    _lactoseFree,
-                    (newValue) {
-                      setState(() {
-                        _lactoseFree = newValue;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_complete_guide/screens/categories_screen.dart';
 
 import '/screens/filters_screen.dart';
 
